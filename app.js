fd68c17af63ac5afe241c48fda989c21537f5cf3
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-04 11:06:49 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// document.addEventListener("DOMContentLoaded", (e) => {
+//     console.log(e);
+
+// });
+
+
+
+let card = [
+    {
+
+        picture: ""
+
+    },
+    {
+
+        picture: ""
+
+    },
+    {
+
+        picture: ""
+
+    },
+    {
+
+        picture: ""
+
+    },
+    {
+
+        picture: ""
+
+    },
+    {
+
+        picture: ""
+
+    },
+    {
+
+        picture: ""
+
+    },
+    {
+
+        picture: ""
+
+    },
+]
+
+
